55f732b0243acaad6bb6f2c31ea15c92f3c346e9
Matteo Di Iorio <matteodiio941@gmail.com>
2023-06-27 18:52:31 +0200

    WIP: Avatar

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,13 +1,24 @@
 'use client'
 
 import useRoutes from "@/app/hooks/useRoutes";
-
 import { useState } from "react";
-import DesktopItem from "./DesktopItem";
 
-const DesktopSidebar = () => {
+import DesktopItem from "./DesktopItem";
+import { User } from "@prisma/client";
+import Avatar from "../Avatar";
+
+interface DesktopSidebarProps {
+    currentUser: User
+}
+
+const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
+    currentUser
+}) => {
     const routes = useRoutes();
     const [isOpen, setIsOpen] = useState(false);
+
+    console.log({ currentUser })
+
     return (
         <div
             className="
@@ -50,6 +61,26 @@ const DesktopSidebar = () => {
                         />
                     ))}
                 </ul>
+            </nav>
+            <nav
+                className="
+                        mt-4
+                        flex
+                        flex-col
+                        justify-between
+                        items-center
+                "
+            >
+                <div
+                    onClick={() => setIsOpen(true)}
+                    className="
+                        cursor-pointer
+                        hover:opacity-75
+                        transition
+                    "
+                >
+                    <Avatar user={currentUser}/>
+                </div>
             </nav>
         </div>
     );
